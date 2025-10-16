69b3b848ec9470299d1f7922f202adac573755fb
hmidani abdelilah <39446276+hmidani-abdelilah@users.noreply.github.com>
2025-10-16 10:39:11 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,10 +2,21 @@ import os, sys
 
 def resource_path(relative_path):
     """
-    إرجاع المسار الصحيح للملفات سواء عند التشغيل من PyInstaller أو بشكل عادي
+    إرجاع المسار الصحيح للملفات سواء أثناء التطوير أو بعد التحزيم بـ PyInstaller.
+
+    Args:
+        relative_path (str): المسار النسبي للملف داخل المشروع.
+    Returns:
+        str: المسار الكامل (المطلق) للملف.
+    
+    الاستخدام:
+    ----------
+    >>> resource_path("assets/icon.png")
+    ----------
     """
     try:
         base_path = sys._MEIPASS  # إذا البرنامج مجمع
     except Exception:
         base_path = os.path.abspath(".")  # إذا شغال عادي
     return os.path.join(base_path, relative_path)
+
