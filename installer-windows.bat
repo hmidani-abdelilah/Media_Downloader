593be2b0cf8c90cc15b94cf409f5075e2c7ec5a2
hmidani abdelilah <39446276+hmidani-abdelilah@users.noreply.github.com>
2026-06-11 13:44:01 +0100

    Add files via upload

---
+++ installer-windows.bat
@@ -0,0 +1,56 @@
+@echo off
+title Media Downloader installer 
+color 0A 
+
+:: ===== Request Admin Rights =====
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+ echo Requesting Administrator privileges...
+ powershell -Command "Start-Process '%~f0' -Verb RunAs"
+ exit
+)
+
+:: ===== Force le retour au dossier d'origine du script =====
+cd /d "%~dp0"
+
+winget source reset --force
+
+
+echo Installation ou Mise a jour de FFmpeg...
+:: Tente d'abord de mettre à jour, si échec (non installé), installe le programme
+winget upgrade -e --id Gyan.FFmpeg
+if %errorlevel% neq 0 (
+    echo Tentative d'installation de FFmpeg...
+    winget install -e --id Gyan.FFmpeg
+)
+if %errorlevel% neq 0 (echo Echec pour FFmpeg. & pause )
+
+echo Installation ou Mise a jour de Python...
+winget upgrade -e --id Python.Python.3.14
+if %errorlevel% neq 0 (
+    echo Tentative d'installation de Python...
+    winget install -e --id Python.Python.3.14
+)
+if %errorlevel% neq 0 (echo Echec pour Python. & pause )
+
+echo Rechargement dynamique du PATH...
+for /f "tokens=2*" %%a in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set "syspath=%%b"
+for /f "tokens=2*" %%a in ('reg query "HKCU\Environment" /v Path') do set "userpath=%%b"
+set "PATH=%syspath%;%userpath%"
+
+echo Mise a jour de pip...
+python -m pip install --upgrade pip
+
+echo.
+echo Installation des dependances Python...
+if exist "requirements.txt" (
+    pip install --upgrade -r requirements.txt
+) else (
+    echo Erreur : Le fichier requirements.txt est introuvable dans le dossier du script.
+    set ERRORLEVEL=1
+)
+if %errorlevel% neq 0 (echo Echec de l'installation des packages pip. & pause )
+
+echo Tous les packages sont a jour !
+echo Executez run.bat pour lancer le programme.
+pause
